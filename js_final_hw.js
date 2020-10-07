c1478be96b3ce868a208fb36efc2bfb3256a6ff5
jeanfq400 <62359619+jeanfq400@users.noreply.github.com>
2020-10-06 20:59:15 -0400

    Add files via upload

---
+++ js_final_hw.js
@@ -0,0 +1,19 @@
+function billingFunction(){
+	var shipname = document.getElementById("shippingName").value;
+	var shipzip = document.getElementById("shippingZip").value;
+
+	if (document.getElementById('same').checked){
+		document.getElementById('billingName').value = shipname;
+		document.getElementById('billingZip').value = shipzip;
+	}
+
+
+    else{
+	document.getElementById("billingName").value = "";
+	document.getElementById("billingZip").value = "";
+}
+}
+/*Add the JavaScript here for the function
+billingFunction().
+It is responsible for setting and clearing the fields in
+ Billing Information */
